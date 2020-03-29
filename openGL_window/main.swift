ae0c577da80ee55cf74811b3e6816d03d254b4e6
Michael Kirschner <mjk.kirschner@gmail.com>
2020-03-29 15:36:15 -0400

    Initial Commit

---
+++ openGL_window/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  openGL_window
+//
+//  Created by Michael Kirschner on 3/29/20.
+//  Copyright © 2020 Michael Kirschner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
